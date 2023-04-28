665fe7ee411c7d14a7b4b8685945a87db97fa221
a1-creator <asbjorn15johnsen@gmail.com>
2023-04-28 20:12:34 +0200

    added background template

---
+++ Scenes/BackgroundScene.py
@@ -0,0 +1,22 @@
+from a1.Scene import Scene
+from a1.GameObject import Alignment, GameObject
+from a1.SpriteTools import SpriteTools
+from a1.ScreenSize import ScreenSize
+
+
+class BackgroundScene(Scene):
+
+    def InitScene(self):
+        super().InitScene()
+        
+        backgroundImage = GameObject(ScreenSize.Center(), "background", Alignment.Center)
+        self.addGameObject(backgroundImage)
+        backgroundImage.addSprite(SpriteTools.getSprite("img/background_ingame.png"))
+
+        
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -6,9 +6,15 @@ import asyncio
 import gameinfo
 
 #Import starting scenes:
-from Scenes.DebugScene import DebugScene
-from Scenes.EmptyScene import EmptyScene
+from Scenes.BackgroundScene import BackgroundScene
 from Scenes.MainScene import MainScene
+from Scenes.EmptyScene import EmptyScene
+from Scenes.DebugScene import DebugScene
+
+def initGameInfo():
+    pygame_icon = pygame.image.load(gameinfo.game_icon_path)
+    pygame.display.set_icon(pygame_icon)
+    pygame.display.set_caption(gameinfo.game_title)
 
 
 def main():
@@ -17,17 +23,14 @@ def main():
 
     pygame.init()
 
-    # --- set Game Icon here ---
-    pygame_icon = pygame.image.load(gameinfo.game_icon_path)
-
-    pygame.display.set_icon(pygame_icon)
-    pygame.display.set_caption(gameinfo.game_title)
-
-    screen = pygame.display.set_mode(ScreenSize.Size())
+    initGameInfo()
     
 
     # Add all the layers you need to be able to switch out here:
-    SceneManager.addScene(EmptyScene(), "BackgroundLayer")
+    # If you need to switch a layer in between others later on, you need to define the layer with an empty scene, as so:
+    # SceneManager.addScene(EmptyScene(), "ExampleLayer")
+
+    SceneManager.addScene(BackgroundScene(), "BackgroundLayer")
     SceneManager.addScene(MainScene(), "MainLayer")
     SceneManager.addScene(EmptyScene(), "UILayer")
 
@@ -36,6 +39,7 @@ def main():
 
     # This starts the while loop that runs the game
     # We use asyncio so that it is easy to convert to a webgame / exe build if you need to.
+    screen = pygame.display.set_mode(ScreenSize.Size())
     asyncio.run( SceneManager.Run(screen) )
 
     pygame.quit()
